24e71ce2ff40ea5bc35f39eb5174e04bbe70c21d
AlFilippov <41473157+alfilippov@users.noreply.github.com>
2019-05-11 19:30:43 +0300

    learning scope bean

--- src/main/java/ru/filippov/springcourse/TestSpring.java
+++ src/main/java/ru/filippov/springcourse/TestSpring.java
@@ -10,8 +10,15 @@ public class TestSpring {
         //Music music = context.getBean("musicBean", Music.class);
         //Внедрение зависимости
        //MusicPlayer musicPlayer = new MusicPlayer(music);
-       MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
-       musicPlayer.playMusic();
+       MusicPlayer firstMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+       MusicPlayer secondMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+    boolean comparison = firstMusicPlayer==secondMusicPlayer;
+        System.out.println(comparison);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+      firstMusicPlayer.setVolume(10);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
         context.close();
     }
 }
